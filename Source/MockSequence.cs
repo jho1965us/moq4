b5434380cb7c0facb339e07564a64661538511fc
Jens <jho.github@gmail.com>
2015-05-07 19:36:21 +0200

    Extended MockSequence to enable a Mock to be part of more than one sequence
    added extension method InSequences

--- Source/MockSequence.cs
+++ Source/MockSequence.cs
@@ -1,4 +1,6 @@
-﻿using Moq.Language;
+﻿using System;
+using System.Linq;
+using Moq.Language;
 
 namespace Moq
 {
@@ -40,6 +42,43 @@ namespace Moq
 			   condition: () => expectationPosition == sequenceStep,
 			   success: NextStep));
 		}
+
+		internal static ISetupConditionResult<TMock> For<TMock>(MockSequence[] sequences, Mock<TMock> mock)
+			where TMock : class
+		{
+			var parts = new Part[sequences.Length];
+			for (int index = 0; index < sequences.Length; index++)
+			{
+				var sequence = sequences[index];
+				parts[index] = new Part(sequence.sequenceLength++, sequence);
+			}
+
+			return mock.When(new Condition(
+			   condition: () => parts.All(part => part.Condition()),
+			   success: () => Array.ForEach(parts, part => part.NextStep())));
+		}
+
+		struct Part
+		{
+			private int expectationPosition;
+			private MockSequence sequence;
+
+			public Part(int expectationPosition, MockSequence sequence) : this()
+			{
+				this.expectationPosition = expectationPosition;
+				this.sequence = sequence;
+			}
+
+			public bool Condition()
+			{
+				return expectationPosition == sequence.sequenceStep;
+			}
+
+			public void NextStep()
+			{
+				sequence.NextStep();
+			}
+		}
 	}
 
 	/// <summary>
@@ -58,6 +97,36 @@ namespace Moq
 			Guard.NotNull(() => sequence, sequence);
 
 			return sequence.For(mock);
+		}
+
+		/// <summary>
+		/// Perform an expectation in the trace.
+		/// </summary>
+		public static ISetupConditionResult<TMock> InSequences<TMock>(
+			this Mock<TMock> mock,
+			params MockSequence[] sequences)
+			where TMock : class
+		{
+			Guard.NotNull(() => sequences, sequences);
+			for (var index = 0; index < sequences.Length; index++)
+			{
+				var sequence = sequences[index];
+				if (sequence == null)
+				{
+					throw new ArgumentException("sequences contains null sequence at position " + index);
+				}
+
+				//for (var other = 0; other < index; other++)
+				//{
+				//    if (sequence == sequences[other])
+				//    {
+				//        throw new ArgumentException("sequences contains repeated entry at position " + index + " and " + other);
+				//    }
+				//}
+			}
+
+
+			return MockSequence.For(sequences, mock);
 		}
 	}
 }
---
+++ UnitTests/MockSequencesFixture.cs
@@ -0,0 +1,130 @@
+﻿using Xunit;
+
+namespace Moq.Tests
+{
+	public class MockSequencesFixture
+	{
+		[Fact]
+		public void RightSequenceSuccess()
+		{
+			var a = new Mock<IFoo>(MockBehavior.Strict);
+			var b = new Mock<IFoo>(MockBehavior.Strict);
+			var c = new Mock<IFoo>(MockBehavior.Strict);
+
+			var sequence1 = new MockSequence();
+			var sequence2 = new MockSequence();
+			var sequence3 = new MockSequence();
+			a.InSequences(sequence1, sequence2).Setup(x => x.Do(100)).Returns(101);
+			b.InSequences(sequence2, sequence3).Setup(x => x.Do(200)).Returns(201);
+			c.InSequences(sequence1, sequence3).Setup(x => x.Do(300)).Returns(301);
+
+			a.Object.Do(100);
+			b.Object.Do(200);
+			c.Object.Do(300);
+		}
+
+		[Fact]
+		public void InvalidSequenceFail()
+		{
+			var a = new Mock<IFoo>(MockBehavior.Strict);
+			var b = new Mock<IFoo>(MockBehavior.Strict);
+			var c = new Mock<IFoo>(MockBehavior.Strict);
+
+			var sequence1 = new MockSequence();
+			var sequence2 = new MockSequence();
+			var sequence3 = new MockSequence();
+			a.InSequences(sequence1, sequence2).Setup(x => x.Do(100)).Returns(101);
+			b.InSequences(sequence2, sequence3).Setup(x => x.Do(200)).Returns(201);
+			c.InSequences(sequence1, sequence3).Setup(x => x.Do(300)).Returns(301);
+
+			a.Object.Do(100);
+			Assert.Throws<MockException>(() => c.Object.Do(300));
+		}
+
+		[Fact]
+		public void NoCyclicSequenceFail()
+		{
+			var a = new Mock<IFoo>(MockBehavior.Strict);
+			var b = new Mock<IFoo>(MockBehavior.Strict);
+
+			var sequence = new MockSequence();
+			a.InSequences(sequence).Setup(x => x.Do(100)).Returns(101);
+			b.InSequences(sequence).Setup(x => x.Do(200)).Returns(201);
+
+			Assert.Equal(101, a.Object.Do(100));
+			Assert.Equal(201, b.Object.Do(200));
+
+			Assert.Throws<MockException>(() => a.Object.Do(100));
+			Assert.Throws<MockException>(() => b.Object.Do(200));
+		}
+
+		[Fact]
+		public void CyclicSequenceSuccesss()
+		{
+			var a = new Mock<IFoo>(MockBehavior.Strict);
+			var b = new Mock<IFoo>(MockBehavior.Strict);
+
+			var sequence = new MockSequence { Cyclic = true };
+			a.InSequences(sequence).Setup(x => x.Do(100)).Returns(101);
+			b.InSequences(sequence).Setup(x => x.Do(200)).Returns(201);
+
+			Assert.Equal(101, a.Object.Do(100));
+			Assert.Equal(201, b.Object.Do(200));
+
+			Assert.Equal(101, a.Object.Do(100));
+			Assert.Equal(201, b.Object.Do(200));
+
+			Assert.Equal(101, a.Object.Do(100));
+			Assert.Equal(201, b.Object.Do(200));
+		}
+
+		[Fact]
+		public void SameMockRightSequenceConsecutiveInvocationsWithSameArguments()
+		{
+			var a = new Mock<IFoo>(MockBehavior.Strict);
+
+			var sequence = new MockSequence();
+			a.InSequences(sequence).Setup(x => x.Do(100)).Returns(101);
+			a.InSequences(sequence).Setup(x => x.Do(100)).Returns(102);
+			a.InSequences(sequence).Setup(x => x.Do(200)).Returns(201);
+			a.InSequences(sequence).Setup(x => x.Do(100)).Returns(103);
+
+			Assert.Equal(101, a.Object.Do(100));
+			Assert.Equal(102, a.Object.Do(100));
+			Assert.Equal(201, a.Object.Do(200));
+			Assert.Equal(103, a.Object.Do(100));
+		}
+
+		[Fact]
+		public void SameMockRightSequenceSuccess()
+		{
+			var a = new Mock<IFoo>(MockBehavior.Strict);
+
+			var sequence = new MockSequence();
+			a.InSequences(sequence).Setup(x => x.Do(100)).Returns(101);
+			a.InSequences(sequence).Setup(x => x.Do(200)).Returns(201);
+
+			Assert.Equal(101, a.Object.Do(100));
+			Assert.Equal(201, a.Object.Do(200));
+			Assert.Throws<MockException>(() => a.Object.Do(100));
+			Assert.Throws<MockException>(() => a.Object.Do(200));
+		}
+
+		[Fact]
+		public void SameMockInvalidSequenceFail()
+		{
+			var a = new Mock<IFoo>(MockBehavior.Strict);
+
+			var sequence = new MockSequence();
+			a.InSequences(sequence).Setup(x => x.Do(100)).Returns(101);
+			a.InSequences(sequence).Setup(x => x.Do(200)).Returns(201);
+
+			Assert.Throws<MockException>(() => a.Object.Do(200));
+		}
+
+		public interface IFoo
+		{
+			int Do(int arg);
+		}
+	}
+}
